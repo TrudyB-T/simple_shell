8ef8728c506ec0eea4f2a09e22c87c12047f867c
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-06-24 23:16:51 -0700

    printenv

---
+++ printenv.c
@@ -0,0 +1,21 @@
+#include "prototypes.h"
+
+/**
+ */
+void _printenv(void)
+{
+	char *env = *environ;
+	int len1, len2;
+
+	for(len1 = 1; env != NULL; len1++)
+	{
+		len2 = 0;
+		while (env[len2])
+		{
+			write(1, &env[len2], 1);
+			len2++;
+		}
+		write(1, "\n", 1);
+		env = environ[len1];
+	}
+}
