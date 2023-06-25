351ca63f4d8af9e0c4529b4da6c42e991c1d1800
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-06-24 19:33:41 -0700

    updates

--- prototypes.h
+++ prototypes.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <sys/stat.h>
 #define MAX_COMMAND 100
 
 #define GNU_SOURCE
@@ -17,9 +17,13 @@
 int isatty(int fd);
 extern char **environ;
 int main(int argc, char **argv, char **env);
+int _strlen(char *s);
 int execute(char **argv);
 char *_getline(void);
 char **tokenize(char *ptr, int bufsize);
 void *reallocate(void *str, unsigned int size1, unsigned int size2);
+char *locate_command(char *argv);
+char *_searchenv(char *env);
+char *_strconcat(char *s1, char *s2);
 
 #endif
