a09ce65ae694a6c176d38df05c7e8e5b3c2c956f
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-06-24 19:20:45 -0700

    update to handle PATH

--- shell.c
+++ shell.c
@@ -1,17 +1,19 @@
 #include "prototypes.h"
 
 /**
- * main - Entry point
+ * main - a simple shell interpreter
  * @argc: argument count
  * @argv: argument vector
- * Return: nothing
+ * Return: 0 means success
  */
 
 int main (int argc, char **argv, char **env)
 {
-	char *str = "trukel$ ";
-	char *line;
+	char *str = (":)trukel$ ");
+	char *line = NULL;
+	int len = _strlen(str);
 	int bufsize = BUFFER_LEN;
+	struct stat fileinfo;
 	(void) argc;
 	(void) env;
 
@@ -20,14 +22,26 @@ int main (int argc, char **argv, char **env)
 	{
 		if (isatty(STDIN_FILENO))
 			
-			write(STDOUT_FILENO, str ,8);
+			write(STDOUT_FILENO, str ,len);
 
 		line = _getline();
 
 	
 		argv = tokenize(line, bufsize);
-		
-		execute(argv);
+		if (argv == NULL || argv[0] == NULL)
+		{
+			execute(argv);
+		}
+		else
+		{
+			if (stat(argv[0], &fileinfo) != 0)
+			{
+				argv[0] = locate_command(argv[0]);
+			}
+			execute(argv);
+		}
+
+
 	}
 	return(0);
 }
