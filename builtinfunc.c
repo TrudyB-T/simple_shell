c791d5ba3eb529634c0adca53ab69421d7449c0e
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-06-24 23:14:40 -0700

    updated with env

--- builtinfunc.c
+++ builtinfunc.c
@@ -1,6 +1,8 @@
 #include "prototypes.h"
 
 /**
+ * _exit_ - executes the exit builtin
+ * @argv: the string to be executed
  */
 
 void _exit_(char **argv)
@@ -10,7 +12,6 @@ void _exit_(char **argv)
 		free(argv);
 		exit(0);
 	}
-
 	else
 	{
 		free(argv);
