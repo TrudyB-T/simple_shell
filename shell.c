6bc65083f961f5421be03c831eee054daad80b30
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-06-26 18:13:38 -0700

    updated shell.c

--- shell.c
+++ shell.c
@@ -1,30 +1,40 @@
 #include "prototypes.h"
 
 /**
- * main - a simple shell interpreter
- * @argc: argument count
- * @argv: argument vector
- * @env: environment
+ * main - run a simple shell interpreter
  * Return: 0 means success
  */
-int main(int argc, char **argv, char **env)
+int main(void)
 {
-	char *str = (":)trukel$ "), *line = NULL;
+	char *str = (":)trukel$ "), *lineptr = NULL, *line, **argv;
 	int len = _strlen(str), k, bufsize = BUFFER_LEN;
+	ssize_t num;
+	size_t s = 0;
 	struct stat fileinfo;
-	(void) argc;
-	(void) env;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
+		if (isatty(STDIN_FILENO) == 1)
 
 			write(STDOUT_FILENO, str, len);
 
-		line = _getline();
+		num = _getline(&lineptr, &s, stdin);
+		if (num == -1)
+		{
+			exit(-1);
+		}
+		if (lineptr[num - 1] == '\n')
+			lineptr[num - 1] = '\0';
+		k = 0;
+		while (lineptr[k])
+		{
+			if (lineptr[k] == '\0')
+				lineptr[k] = 0;
+			k++;
+		}
+		line = lineptr;
 
 		argv = tokenize(line, bufsize);
-
 		if (argv == NULL || argv[0] == NULL)
 		{
 			execute(argv);
@@ -32,18 +42,19 @@ int main(int argc, char **argv, char **env)
 		else
 		{
 			k = search_builtin(argv[0]);
-
 			if (k >= 0)
 				exec_builtins(argv, k);
-
 			else
 			{
 				if (stat(argv[0], &fileinfo) != 0)
 					argv[0] = locate_command(argv[0]);
 				execute(argv);
 			}
+		
 		}
 		free(argv);
+	
 	}
-	return (0);
+	free(lineptr);
+       return (0);
 }
