8136110f62216f39894dfabc6c8fb37475b1114c
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-06-26 18:18:42 -0700

    updates

--- getcommand.c
+++ getcommand.c
@@ -1,29 +1,95 @@
 #include "prototypes.h"
 
 /**
- * _getline - gets the input string from user
- *
- * Return: pointer to the input string
+ * get_lineptr - reassigns the lineptr
+ * @lineptr: a pointer to an array of strings
+ * @s: size of lineptr
+ * @buffer: a buffer to store temporal strings
+ * @c: size of buffer
  */
-
-
-char *_getline(void)
+void get_lineptr(char **lineptr, size_t *s, char *buffer, size_t c)
 {
-	char *lineptr = NULL;
-	size_t n = 0;
-	int num;
-
-
-	num = getline(&lineptr, &n, stdin);
-
-	if (num == -1)
+	if (!lineptr)
 	{
-		if (isatty(STDIN_FILENO))
-			write(1, "\n", 1);
-		exit(0);
+		if (c > 100)
+		{
+			*s = c;
+		}
+		else
+		{
+			*s = 100;
+		}
+		*lineptr = buffer;
 	}
-
-	lineptr = strtok(lineptr, "\n");
-
-	return (lineptr);
+	else if (*s < c)
+	{
+		if (c > 100)
+		{
+			*s = c;
+		}
+		else
+		{
+			*s = 100;
+		}
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
 }
+
+
+	
+
+/**
+ * _getline - gets the input string from user
+ * @lineptr: input string
+ * @s: size of lineptr
+ * @stream: stream to read
+ * Return: number of bytes
+ */
+ssize_t _getline(char **lineptr, size_t *s, FILE *stream)
+{
+	ssize_t num;
+	char b = 'x', *buffer;
+	int n;
+	static ssize_t rec;
+
+	if (!rec)
+		fflush(stream);
+	else
+		return (-1);
+	rec = 0;
+	buffer = malloc(sizeof(char) * 100);
+	if (buffer == NULL)
+		return (-1);
+	while (b != '\n')
+	{
+		n = read(STDIN_FILENO, &b, 1);
+		if (n == -1 || (n == 0 && rec == 0))
+		{
+			free(buffer);
+			return(-1);
+		}
+		if (n == 0 && rec != 0)
+		{
+			rec++;
+			break;
+		}
+		if (rec >= 100)
+			buffer = reallocate(buffer, rec, rec + 1);
+		buffer[rec] = b;
+		rec++;
+	}
+	buffer[rec] = '\0';
+
+	get_lineptr(lineptr, s, buffer, rec);
+
+	num = rec;
+	if (n != 0)
+		rec = 0;
+	return (num);
+}
+
--- stringtokenizer.c
+++ stringtokenizer.c
@@ -11,9 +11,8 @@
 
 char **tokenize(char *ptr, int bufsize)
 {
-	int k = 0;
-	char *tok = NULL;
-	char **argv;
+	int k = 0, tok_len;
+	char *end_tok, **argv;
 
 
 	argv = malloc(sizeof(char *) * bufsize);
@@ -22,24 +21,33 @@ char **tokenize(char *ptr, int bufsize)
 	{
 		perror("./hsh");
 		free(argv);
-		exit(0);
+		exit(0);	
 	}
-
-	tok = strtok(ptr, " ");
-
-	while (tok)
+	while (*ptr != '\0')
 	{
-		argv[k] = tok;
-		tok = strtok(NULL, " ");
+		while (*ptr == ' ')
+			ptr++;
+		if (*ptr == '\0')
+			break;
+		end_tok = ptr;
+		while(*end_tok != '\0' && *end_tok != ' ')
+			end_tok++;
+		tok_len = end_tok - ptr;
+		argv[k] = malloc(tok_len + 1);
+		if (argv[k] == NULL)
+		{
+			perror("./hsh");
+			exit(0);
+		}
+		_strncpy(argv[k], ptr, tok_len);
+		argv[k][tok_len] = '\0';
+		ptr = end_tok;
 		k++;
 
 		if (k >= bufsize)
 		{
 			bufsize += BUFFER_LEN;
-
-			argv = reallocate(argv, sizeof(argv), bufsize * sizeof(char *));
-
-
+			argv = reallocate(argv, sizeof(argv), sizeof(char *) * bufsize);
 			if (argv == NULL)
 			{
 				perror("./hsh");
@@ -51,4 +59,27 @@ char **tokenize(char *ptr, int bufsize)
 	argv[k] = NULL;
 
 	return (argv);
+}
+
+
+/**
+ * *_strncpy - copies a string
+ * @dest: string destination
+ * @src: string source
+ * @n: number of bytes
+ * Return: pointer to the resulting dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; (i < n && src[i] != '\0'); i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
